56b38c4df25f2613a81afabac44ebde261df5c91
Jesse Paris-Jourdan <jesseparisjourdan@gmail.com>
2019-09-30 10:55:44 +1000

    set stuff up

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 /*
-  yeah i went into this without thinking at all
-  how are we structuring this
-
-should we add all the links first?
-that way we can first build the logic that actually deals with the kind of link they are
-if they're an image it's pretty clear what to do
-if they're another html page they'll need to be parsed by the xmlhttprequest and i'm not quite sure how to do that yet
-or the html page can probable even be read as text and then searched with regex and read as a document fragment's innerhtml or sth
-
-okay you're going to need
 
 init
 - add event listeners
@@ -17,12 +7,30 @@ init
 global blobs
 
 function for loading images
-
 function for loading text
-
 function for loading in general
 
- */
+*/
+ 
+function init() {
+  console.log("test!");
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 
 function init() {
@@ -77,3 +85,4 @@ function imgLoad(url) {
 
 window.addEventListener("DOMContentLoaded", init);
 
+*/
